f839e0524a4cd1c445d0605efd695813ef24acfe
flutter99 <82260050+flutter99@users.noreply.github.com>
2023-02-04 16:06:52 +0500

    schedule notification added with image
    Schedule norification added by fetching the images and icon from internet

--- local_notification_app/lib/notification_api.dart
+++ local_notification_app/lib/notification_api.dart
@@ -1,9 +1,13 @@
+
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+//import 'package:rxdart/rxdart.dart';
 
 class NotificationApi {
   final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -17,13 +21,16 @@ class NotificationApi {
         requestSoundPermission: true,
         requestBadgePermission: true,
         requestCriticalPermission: true,
-        onDidReceiveLocalNotification:
-            (int id, String? title, String? body, String? payload) async {});
+        onDidReceiveLocalNotification: (int id, String? title, String? body, String? payload) async {
+
+        });
 
     var initializeSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializeSettingsIOS);
 
     await flutterLocalNotificationsPlugin.initialize(initializeSettings,
-        onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) async {});
+        onDidReceiveNotificationResponse: (NotificationResponse notificationResponse) async {
+
+        });
   }
 
 
@@ -59,20 +66,6 @@ class NotificationApi {
 
   var scheduledNotificationDateTime = DateTime.now().add(const Duration(seconds: 3));
 
-  // static Future<String> getImageFilePathFromAssets(String asset) async {
-  //   final byteData = await rootBundle.load(asset);
-  //
-  //   final file =
-  //   File('${(await getTemporaryDirectory()).path}/${asset.split('/').last}');
-  //   await file.writeAsBytes(byteData.buffer
-  //       .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
-  //   return file.path;
-  // }
-
-  // final attachmentPicturePath = await getImageFilePathFromAssets('assets/img.png');
-  // final BigPictureStyleInformation bigPictureStyleInformation =
-  // BigPictureStyleInformation(FilePathAndroidBitmap(attachmentPicturePath),
-  //     largeIcon: FilePathAndroidBitmap(filePath));
 
   /// show schedule notification
   Future showScheduleNotification({
@@ -90,8 +83,17 @@ class NotificationApi {
     );
   }
 
-  scheduleNotificationDetails() {
-    return const NotificationDetails(
+  scheduleNotificationDetails() async {
+
+    final largeIconPath = await Utils.downlaodFile('https://cdn.pixabay.com/photo/2013/07/13/13/53/diamond-161739_960_720.png', 'largeIcon');
+    final bigPicPath = await Utils.downlaodFile('https://cdn.pixabay.com/photo/2022/11/20/09/06/night-view-7603847_960_720.jpg', 'bigPic');
+
+    final styleInformation = BigPictureStyleInformation(
+      FilePathAndroidBitmap(bigPicPath),
+      largeIcon: FilePathAndroidBitmap(largeIconPath),
+    );
+
+    return NotificationDetails(
         android: AndroidNotificationDetails(
           'channelId',
           'channelName',
@@ -112,9 +114,23 @@ class NotificationApi {
 
           /// sound path
           sound: RawResourceAndroidNotificationSound('notisound'),
+
+          styleInformation: styleInformation,
           importance: Importance.max,
           priority: Priority.high,
         ),
         iOS: DarwinNotificationDetails());
+  }
+}
+
+class Utils {
+  static Future<String> downlaodFile(String url, String fileName) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$fileName';
+    final response = await http.get(Uri.parse(url));
+    final file = File(filePath);
+
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
   }
 }
